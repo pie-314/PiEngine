9a98a539c5c5725c7f0a42a0e280da2bcecf1b97
yashksaini-coder <yashksaini89@gmail.com>
2026-04-06 00:19:38 +0530

    Add strcmp for digits argument

--- src/main.c
+++ src/main.c
@@ -1,6 +1,9 @@
 #include "chudnovsky.h"
+#include "cli.h"
 #include <mpfr.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // Constant terms
 #define A 13591409
@@ -12,8 +15,25 @@
 
 void chudnovsky_term(mpfr_t term, int k);
 
-int main() {
+int main(int argc, char *argv[]) {
   int digits = 1000;
+
+  // Parse arguments (start from 1 to skip program name)
+  for (int i = 1; i < argc; i++) {
+    printf("argument %d is %s\n", i, argv[i]);
+
+    // strcmp returns 0 when equal, so check for == 0
+    if (strcmp(argv[i], "--digits") == 0 || strcmp(argv[i], "-d") == 0) {
+      if (i + 1 < argc) {
+        digits = atoi(argv[++i]); // Parse the next argument as integer
+        printf("Input is a digit: %d\n", digits);
+      } else {
+        fprintf(stderr, "Error: --digits requires a value\n");
+        return 1;
+      }
+    }
+  }
+
   set_precision(digits);
 
   mpfr_t pi;
